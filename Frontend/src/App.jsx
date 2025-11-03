6f85f2abfc397cdf60b7352952bdb887fad6c5e7
Arhan Alam <arhanalam@Arhans-MacBook-Pro.local>
2025-11-04 02:25:25 +0530

    home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
