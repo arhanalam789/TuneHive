bf28288e90e1ce7403c724a5d0cbe6538c000360
Arhan Alam <arhanalam@Arhans-MacBook-Pro.local>
2025-11-09 01:44:29 +0530

    corss

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,18 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
